d1645c50cd3badf6f197f4211cd13f06188f5d40
Tiffanie <tiffanielchau@gmail.com>
2021-07-29 19:02:42 -0400

    some React index and choosePath

--- food-recommendations-app/src/index.js
+++ food-recommendations-app/src/index.js
@@ -4,9 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Index extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="front-background-image">
+        <div className="font-page-text">
+          <h1 id = "Title"> Bears Eatin Good</h1>
+          <div className ="frontPageDescription">
+            <p>Where to eat and what to eat!</p>
+            <p>Codeology Fall 2021 Project</p>
+            <p>Sort and shift through CalDining options and the endless restaurants around campus</p>
+          </div>
+          <button type = "button" id = "frontGoButton">
+            Lets Eat!
+          </button>
+        </div>
+      </div>
+    );}
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );

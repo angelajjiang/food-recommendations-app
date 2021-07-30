7f3da2b9794bf8958680414f3515e0309826834c
Angela Jiang <ajjiang@berkeley.edu>
2021-07-29 18:02:33 -0700

    moves from start page to choice page

--- food-recommendations-app/src/App.js
+++ food-recommendations-app/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import ChoosePath from './ChoosePath';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      start: true
+    };
+    this.getStarted = this.getStarted.bind(this);
+  }
+  
+  async getStarted(event) {
+    this.setState({start: false});
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div> {this.state.start ?
+        <div className="front-background-image">
+          <div className="font-page-text">
+            <h1 id = "Title"> Bears Eatin Good</h1>
+            <div className ="frontPageDescription">
+              <p>Where to eat and what to eat!</p>
+              <p>Codeology Fall 2021 Project</p>
+              <p>Sort and shift through CalDining options and the endless restaurants around campus</p>
+            </div>
+            <button type = "button" id = "frontGoButton" onClick={this.getStarted}>
+              Lets Eat!
+            </button>
+          </div>
+        </div>
+      : <ChoosePath/>
+    } </div>
+  );}
 }
 
 export default App;
--- food-recommendations-app/src/choosePath.js
+++ food-recommendations-app/src/choosePath.js
@@ -4,47 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-class choosePath extends React.Component {
+class ChoosePath extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      dininghall: false,
+      restaurant: false
+    };
   }
   render(){
     return (
-      <div className="topnav">
-        <li style="float:left"><a href="index.html">Home</a></li>
-        <li style="float:left"><a href="CalDining.html">CalDining</a></li>
-        <li style="float:left"><a href="AroundBerkeley.html">Around Berkeley</a></li>
-        <li style="float:right"><a href="#profile">Profile</a></li>
-      </div>
-      <h2 class="BerkeleyEatinGood">Berkeley Eatin Good</h2>
-      <p id = "chooseDescription">Choose one of the options below :)</p>
-      <div className = "imageOptions">
-        <div className ="CalDining-image">
-          <a href="CalDining.html">
-            <img border="0" alt="CalDining" src="food spread bowls.jpg" width="450" height="350">
-          </a>
-          <h3 className = "imageLabel">CalDining</h3>
+      <div>
+        <div className="topnav">
+          <li style={{float:'left'}}><a href="index.html">Home</a></li>
+          <li style={{float:'left'}}><a href="CalDining.html">CalDining</a></li>
+          <li style={{float:'left'}}><a href="AroundBerkeley.html">Around Berkeley</a></li>
+          <li style={{float:'right'}}><a href="#profile">Profile</a></li>
         </div>
+        <h2 class="BerkeleyEatinGood">Berkeley Eatin Good</h2>
+        <p id = "chooseDescription">Choose one of the options below :)</p>
+        <div className = "imageOptions">
+          <div className ="CalDining-image">
+            <a href="CalDining.html">
+              <img border="0" alt="CalDining" src="food spread bowls.jpg" width="450" height="350"/>
+            </a>
+            <h3 className = "imageLabel">CalDining</h3>
+          </div>
 
-        <div className ="AroundBerkeley-image">
-          <a href="AroundBerkeley.html">
-            <img border="0" alt="Around Berkeley" src="breakfast spread.jpg" width="450" height="350">
-          </a>
-          <h3 className = "imageLabel"> Around Berkeley</h3>
+          <div className ="AroundBerkeley-image">
+            <a href="AroundBerkeley.html">
+              <img border="0" alt="Around Berkeley" src="breakfast spread.jpg" width="450" height="350"/>
+            </a>
+            <h3 className = "imageLabel"> Around Berkeley</h3>
+          </div>
         </div>
       </div>
     );}
 }
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <choosePath />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default ChoosePath;

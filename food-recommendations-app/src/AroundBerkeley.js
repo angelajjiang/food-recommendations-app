3bf0711c143a1eca7f54803dc41f4d0c0019b4da
Angela Jiang <ajjiang@berkeley.edu>
2021-08-01 13:48:14 -0700

    filtering form

--- food-recommendations-app/src/AroundBerkeley.js
+++ food-recommendations-app/src/AroundBerkeley.js
@@ -7,9 +7,40 @@ class AroundBerkeley extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      yelpData: null
+      yelpData: null,
+      type: null,
+      method: null,
+      price: null,
+      rating: null,
+      distance: null
     };
+    this.setType = this.setType.bind(this);
+    this.setMethod = this.setMethod.bind(this);
+    this.setPrice = this.setPrice.bind(this);
+    this.setRating = this.setRating.bind(this);
+    this.setDistance = this.setDistance.bind(this);
     this.getRestaurant = this.getRestaurant.bind(this);
+  }
+
+  setType(event) {
+    event.preventDefault();
+    this.setState({type: event.target.value});
+  }
+  setMethod(event) {
+    event.preventDefault();
+    this.setState({method: event.target.value});
+  }
+  setPrice(event) {
+    event.preventDefault();
+    this.setState({price: event.target.value});
+  }
+  setRating(event) {
+    event.preventDefault();
+    this.setState({rating: event.target.value});
+  }
+  setDistance(event) {
+    event.preventDefault();
+    this.setState({distance: event.target.value});
   }
 
   async getRestaurant(event) {
@@ -28,6 +59,28 @@ class AroundBerkeley extends React.Component {
   render() {
     return (
       <div>
+        <form>
+          <textarea value={this.state.type} onChange={this.state.setType}>What kind of food?</textarea>
+          <select value={this.state.method} onChange={this.state.setMethod}>
+            <option value="dine-in">Dine-in</option>
+            <option value="takeout">Takeout</option>
+            <option value="delivery">Delivery</option>
+          </select>
+          <select value={this.state.price} onChange={this.state.setPrice}>
+            <option value="$">$</option>
+            <option value="$$">$$</option>
+            <option value="$$$">$$$</option>
+            <option value="$$$$">$$$$</option>
+          </select>
+          <select value={this.state.rating} onChange={this.state.setRating}>
+            <option value="1">1 star</option>
+            <option value="2">2 star</option>
+            <option value="3">3 star</option>
+            <option value="4">4 star</option>
+            <option value="5">5 star</option>
+          </select>
+          <textarea value={this.state.distance} onChange={this.state.setDistance}>Distance</textarea>
+        </form>
         <button type = "button" id = "frontGoButton" onClick={this.getRestaurant}>
           Get suggestion
         </button>
